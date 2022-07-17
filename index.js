2caf83ec3320333da2c265fc16cfbef9626a33c0
Hero <76834176+jish0101@users.noreply.github.com>
2022-07-17 23:37:41 +0530

    Add files via upload
    that's all!

---
+++ index.js
@@ -0,0 +1,25 @@
+// Increment Function
+
+let countVar = 0;
+
+function increment() {
+    countVar = countVar + 1;
+    document.getElementById("count").innerText = countVar;
+}
+
+// Decrement Function
+
+function decrement() {
+    if (countVar > 0) {
+
+        countVar = countVar - 1;
+        document.getElementById("count").innerText = countVar;
+    }
+}
+
+// Reset Function
+
+function reset() {
+    document.getElementById("count").innerText = 0;
+    countVar = 0;
+}
